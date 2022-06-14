9fb5e81c1d8f746c566b7af246293795c787fc74
Irsandi Age Pangestu <irsandiagep@gmail.com>
2022-06-15 01:03:49 +0700

    wes mboh

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import tempfile
 import time
 import pickle
 import pandas as pd
-from sklearn.model_selection import train_test_split
+"""from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
 from sklearn.linear_model import LogisticRegression, RidgeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier"""
 from PIL import Image
 
 mp_drawing = mp.solutions.drawing_utils
